8dc43c8ef0f3fcb3d87b00a2fc8131b7bbb6a25c
fireproofsocks <fireproofsocks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-25 22:24:19 +0000

    Saving work...
    git-svn-id: https://plugins.svn.wordpress.org/custom-content-type-manager/trunk@455714 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ $class_names_used -- add any class names that are declared by this plugin.
 Warning: the text-domain for the __() localization functions is hardcoded.
 ------------------------------------------------------------------------------*/
 $function_names_used = array('get_custom_field','get_custom_field_meta','get_custom_field_def'
-	,'get_all_fields_of_type','get_post_complete','get_posts_sharing_custom_field_value'
+	,'get_post_complete','get_posts_sharing_custom_field_value'
 	,'get_relation','get_unique_values_this_custom_field','print_custom_field','print_custom_field_meta'
 	,'uninstall_cctm');
 $class_names_used = array('CCTM', 'ImportExport'
