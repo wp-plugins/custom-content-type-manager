8efbd668444f77ceafabbb8b5f7824f133e0b52d
fireproofsocks <fireproofsocks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-21 00:43:42 +0000

    Issue 443
    git-svn-id: https://plugins.svn.wordpress.org/custom-content-type-manager/trunk@685001 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/CCTM_Columns.php
+++ includes/CCTM_Columns.php
@@ -2,39 +2,48 @@
 /**
  * This handles custom columns when creating lists of posts/pages in the manager.
  * We use an object here so we can rely on a "dynamic funciton name" via __call() where
- * the function called corresponds to the post-type name. 
+ * the function called corresponds to the post-type name.
  *
  * WARNING: this requires that the post-type is named validly, i.e. in a name that would
  * be valid as a PHP function.
- * See 
- *	http://codex.wordpress.org/Plugin_API/Action_Reference/manage_posts_custom_column
- *	http://codex.wordpress.org/Plugin_API/Filter_Reference/manage_edit-post_type_columns
+ * See
+ * http://codex.wordpress.org/Plugin_API/Action_Reference/manage_posts_custom_column
+ * http://codex.wordpress.org/Plugin_API/Filter_Reference/manage_edit-post_type_columns
  *
  * In WP 3.1, manage_edit-${post_type}_columns has been supplanted by manage_${post_type}_posts_columns.
+ *
+ * @package
  */
+
+
 class CCTM_Columns {
 
 	/**
 	 * Sets the post-type, e.g. 'books'
 	 */
-	public $post_type; 
-	
+	public $post_type;
+
+
+
 	/**
 	 * Goes to true if the custom columns do not include the built-in title field
 	 */
 	public $no_title_flag = false;
-	
+
 	// Count as we iterate over columns in each row
 	public $new_row = true;
 	public $last_post;
-	
+
+
+
 	/**
 	 * This is the magic function, named after the post-type in question.
-	 * Return value is an associative array where the element key is the name of the column, 
+	 * Return value is an associative array where the element key is the name of the column,
 	 * and the value is the header text for that column.
-	 * @param	string	$post_type
-	 * @param	array	$default_columns associative array (set by WP);
-	 * @return	array	associative array of column ids and translated names for header names.
+	 *
+	 * @param string  $post_type
+	 * @param array   $default_columns associative array (set by WP);
+	 * @return array associative array of column ids and translated names for header names.
 	 */
 	public function __call($post_type, $default_columns) {
 		$custom_columns = array('cb' => '<input type="checkbox" />');
@@ -76,7 +85,8 @@ class CCTM_Columns {
 		}
 		return $custom_columns;
 	}
-	
+
+
 	//------------------------------------------------------------------------------
 	/**
 	 * Populate the custom data for a given column.  This function should actually
@@ -85,29 +95,42 @@ class CCTM_Columns {
 	 * Note that things get all broken and wonky if you do not include the post title column,
 	 * so this function has some customizations here to print out the various eye-candy
 	 * Edit/Trash/View links in that case.
+	 * See https://code.google.com/p/wordpress-custom-content-type-manager/issues/detail?id=443
 	 *
+	 * @param string $column
 	 */
 	public function populate_custom_column_data($column) {
 
 		global $post;
-		
+
 		if ($this->last_post != $post->ID) {
 			$this->new_row = true;
 		}
-		
-		if ($this->no_title_flag && $this->new_row) {			
-			print '<strong><a class="row-title" href="post.php?post='.$post->ID.'&amp;action=edit">';
+
+		if ($this->no_title_flag && $this->new_row) {
+			printf('<strong><a class="row-title" href="post.php?post=%s&amp;action=edit">',$post->ID);
 		}
 
-		print_custom_field($column);
-		
+		$meta = get_custom_field_meta($column);
+		if ( $meta['type'] === "image" ) {
+			$id = get_custom_field($column.":raw");
+			printf('<a target="_blank" href="%s">%s</a>',get_edit_post_link($id),wp_get_attachment_image($id, "thumbnail"));
+		}
+		elseif ($meta['type'] === "relation" ) {
+			$id = get_custom_field($column.":raw");
+			printf('<a target="_blank" href="%s">%s</a>',get_edit_post_link($id), get_the_title($id));
+		}
+		else {
+			print_custom_field($column);
+		}
+
 		if ($this->no_title_flag && $this->new_row) {
 			print '</a></strong>
 				<div class="row-actions"><span class="edit"><a href="post.php?post='.$post->ID.'&amp;action=edit" title="'.__('Edit').'">'.__('Edit').'</a> | </span>
 				<span class="inline hide-if-no-js"><a href="#" class="editinline">'.__('Quick Edit').'</a> | </span><span class="trash"><a class="submitdelete" href="'.get_delete_post_link($post->ID).'">'.__('Trash').'</a> | </span><span class="view"><a href="'.get_permalink($post->ID).'" rel="permalink">'.__('View').'</a></span>';
 			$this->new_row = false;
 			$this->last_post = $post->ID;
-		}		
+		}
 	}
 }
 /*EOF*/
