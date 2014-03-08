09eca7a3e7891441498d81a3551f90bd38cb118c
fireproofsocks <fireproofsocks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-08 05:37:28 +0000

    Fixed issues with dropdowns/radios
    git-svn-id: https://plugins.svn.wordpress.org/custom-content-type-manager/trunk@871678 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: Custom Content Type Manager : Advanced Custom Post Types
 Description: Allows users to create custom post types and custom fields, including dropdowns, checkboxes, and images. This gives WordPress CMS functionality making it easier to use WP for eCommerce or content-driven sites.
 Author: Everett Griffiths
-Version: 0.9.7.12
+Version: 0.9.7.13
 Author URI: http://www.craftsmancoding.com/
 Plugin URI: http://code.google.com/p/wordpress-custom-content-type-manager/
 ------------------------------------------------------------------------------*/
